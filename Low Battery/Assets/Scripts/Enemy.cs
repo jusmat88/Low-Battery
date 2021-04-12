da9cbad98fb73d1c8c115e806d7e0e3faa7d99ee
jusmat88 <82360044+jusmat88@users.noreply.github.com>
2021-04-12 13:42:16 +0300

    Enemy Base Class
    State has to be changed, Rework ahead.

--- Low Battery/Assets/Scripts/Enemy.cs
+++ Low Battery/Assets/Scripts/Enemy.cs
@@ -6,25 +6,113 @@ public class Enemy : MonoBehaviour
 {
     Rigidbody2D rigidBody;
     BoxCollider2D boxCollider;
+    [SerializeField] private float reactionDistance;
+    [SerializeField] private float catchDistance;
+    [SerializeField] private Transform player;
+    private const string IDLE = "Idle";
+    private const string CHASING = "Chasing";
+    private const string CATCHING = "Catching";
+    [SerializeField] private string currentState;
+
+    bool isChasing = false;
+    bool isCatching = false;
+    bool isIdle = true;
+
+    bool isConcious = true;
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, reactionDistance);
+    }
 
     private void Start() => Initialize();
+
+    private void FixedUpdate()
+    {
+        if (!isConcious) { return; }
+        Vector3 playerDirection = ((player.position + new Vector3(0, 0.25f, 0)) - transform.position).normalized;
+        RaycastHit2D ray = Physics2D.Raycast(transform.position + new Vector3(0,0.25f,0), playerDirection, reactionDistance);
+        
+        float distance = Vector3.Distance(transform.position, player.position);
+
+        Debug.DrawRay(transform.position + new Vector3(0, 0.15f, 0), playerDirection * reactionDistance);
+
+        if (ray.collider.GetComponent<PlayerController>())
+        {
+            if (distance < reactionDistance)
+            {
+                if (distance > catchDistance)
+                {
+                    ChangeState(CHASING);
+                }
+                
+                if (distance <= catchDistance)
+                {
+                    ChangeState(CATCHING);
+                }
+            }
+        }
+        else
+        {
+            ChangeState(IDLE);
+        }
+    }
 
     private void Initialize() {
         rigidBody = GetComponent<Rigidbody2D>();
         boxCollider = GetComponent<BoxCollider2D>();
+        ChangeState(IDLE);
+        player = FindObjectOfType<PlayerController>().transform;
     }
 
     public void Knockout() {
-        print("Knocked out!");
         //Play Knockout Animation
         rigidBody.bodyType = RigidbodyType2D.Static;
         boxCollider.enabled = false;
+        isConcious = false;
     }
 
     IEnumerator IdleAction()
     {
-        yield return new WaitForSeconds(0f);
+        isIdle = true;
+        //Play Idle Animation
+        yield return new WaitForSeconds(2f);
+        StartCoroutine(IdleAction());
 
+    }
 
+    IEnumerator ChaseAction()
+    {
+        isChasing = true;
+        yield return new WaitForSeconds(1f);
+    }
+
+    IEnumerator CatchAction()
+    {
+        //Play Catch Animation
+        isCatching = true;
+        yield return new WaitForSeconds(1f);
+    }
+
+    private void ChangeState(string _newState)
+    {
+        if (_newState == currentState) { return; }
+        currentState = _newState;
+        StopAllCoroutines();
+
+        switch (currentState)
+        {
+            case IDLE:
+                StartCoroutine(IdleAction());
+                break;
+            case CHASING:
+                StartCoroutine(ChaseAction());
+                break;
+            case CATCHING:
+                StartCoroutine(CatchAction());
+                break;
+            default:
+                return;
+        }
     }
 }
